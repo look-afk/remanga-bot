40c36229dc05295d46614dac73c26acfea5ea497
Look__ <zavlatkamalov@gmail.com>
2026-09-06 16:50:32 +0500

    Refactor file writing to use formatted strings

--- botq.py
+++ botq.py
@@ -6,12 +6,8 @@ telegram_token=os.getenv("TELEGRAM_BOT_TOKEN"),
 chat_id=os.getenv("TELEGRAM_CHAT_ID"),
 proxy=os.getenv("CUSTOM_PROXY"),
 with open("m.txt","w+") as f:
-    f.write(email)
-    f.write("lol")
-    f.write(password)
-    f.write("lol")
-    f.write(telegram_token)
-    f.write("lol")
-    f.write(chat_id)
-    f.write("lol")
-    f.write(proxy)
+    f.write(f"{email}\n")
+    f.write(f"{password}\n")
+    f.write(f"{telegram_token}\n")
+    f.write(f"{chat_id}\n")
+    f.write(f"{proxy}\n")
